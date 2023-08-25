49670d0618e6166e67d4ff3aae17a6aae24caacc
Brian Sayler <saylerb@users.noreply.github.com>
2023-08-25 09:08:06 -0600

    [Brian] try camelCase for github encoded json

--- data-source.tf
+++ data-source.tf
@@ -21,6 +21,19 @@ resource "grafana_data_source" "prometheus" {
   })
 }
 
+resource "grafana_data_source" "github" {
+  type                = "grafana-github-datasource"
+  name                = "grafana-github-datasource"
+
+  json_data_encoded = jsonencode({
+    githubUrl        = "github.com"
+  })
+
+  secure_json_data_encoded = jsonencode({
+    accessToken = var.github_access_token
+  })
+}
+
 resource "grafana_cloud_plugin_installation" "github" {
   stack_slug = var.stack_slug
   version    = "1.4.7"
